38e41f8e89a8f097ee39d4ce5b36813b3317c9e3
yukirin <standupdown@gmail.com>
2017-04-30 23:28:12 +0900

    Add ch03

--- ch03/mnist_show.py
+++ ch03/mnist_show.py
@@ -1,14 +1,16 @@
 # coding: utf-8
-import sys, os
-sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
+import sys
+import os
 import numpy as np
-from dataset.mnist import load_mnist
 from PIL import Image
+sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
+from dataset.mnist import load_mnist
 
 
 def img_show(img):
     pil_img = Image.fromarray(np.uint8(img))
     pil_img.show()
+
 
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
 
@@ -20,4 +22,8 @@ print(img.shape)  # (784,)
 img = img.reshape(28, 28)  # 形状を元の画像サイズに変形
 print(img.shape)  # (28, 28)
 
+print(x_train.shape)
+print(t_train.shape)
+print(x_test.shape)
+print(t_test.shape)
 img_show(img)
