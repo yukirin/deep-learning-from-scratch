c4cfdc6ab97451d18204d2677b521ade47b24fac
yukirin <standupdown@gmail.com>
2017-05-04 03:03:03 +0900

    Add ch05

--- ch06/weight_init_activation_histogram.py
+++ ch06/weight_init_activation_histogram.py
@@ -13,7 +13,8 @@ def ReLU(x):
 
 def tanh(x):
     return np.tanh(x)
-    
+
+
 input_data = np.random.randn(1000, 100)  # 1000個のデータ
 node_num = 100  # 各隠れ層のノード（ニューロン）の数
 hidden_layer_size = 5  # 隠れ層が5層
@@ -23,17 +24,16 @@ x = input_data
 
 for i in range(hidden_layer_size):
     if i != 0:
-        x = activations[i-1]
+        x = activations[i - 1]
 
     # 初期値の値をいろいろ変えて実験しよう！
     w = np.random.randn(node_num, node_num) * 1
+    # w = np.random.randn(node_num, node_num) * 0
     # w = np.random.randn(node_num, node_num) * 0.01
     # w = np.random.randn(node_num, node_num) * np.sqrt(1.0 / node_num)
     # w = np.random.randn(node_num, node_num) * np.sqrt(2.0 / node_num)
 
-
     a = np.dot(x, w)
-
 
     # 活性化関数の種類も変えて実験しよう！
     z = sigmoid(a)
@@ -44,10 +44,10 @@ for i in range(hidden_layer_size):
 
 # ヒストグラムを描画
 for i, a in activations.items():
-    plt.subplot(1, len(activations), i+1)
-    plt.title(str(i+1) + "-layer")
+    plt.subplot(1, len(activations), i + 1)
+    plt.title(str(i + 1) + "-layer")
     if i != 0: plt.yticks([], [])
     # plt.xlim(0.1, 1)
     # plt.ylim(0, 7000)
-    plt.hist(a.flatten(), 30, range=(0,1))
+    plt.hist(a.flatten(), 30, range=(0, 1))
 plt.show()
